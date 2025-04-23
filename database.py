1f9cd231762350871577dccc09823aa350cd019f
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-04-23 23:21:42 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 database_url = os.environ.get("DATABASE_URL")
-
 engine = create_engine(database_url, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
